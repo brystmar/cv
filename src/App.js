d401ab7eef15aecafe2c678494c79b030831ef70
Thomas <brystmar@users.noreply.github.com>
2020-08-28 18:47:22 -0700

    Favicon madness!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import './styles.sass';
 
 function App() {
     return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './normalize.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
