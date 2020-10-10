fc5ff16d116ae5df47e6a811c7076d2bb6aad603
Thomas <brystmar@users.noreply.github.com>
2020-10-09 20:20:49 -0700

    Minor Contact updates

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,27 +10,26 @@ function Contact() {
             <h1>Contact</h1>
 
             <p className="contact-list-items">
-                <ContactGroup imgSource="%PUBLIC_URL%/icons/email.svg"
+                <ContactGroup imgSource="../../public/icons/email.svg"
                               imgDescription="Send Thomas an email"
                               url="mailto:tp.berg+cv@gmail.com"
                               text="Email Me"/>
 
-                <span className="contact-group">
-                    <img src="%PUBLIC_URL%/icons/map-marked-alt-solid.svg"
-                         alt="Where Thomas lives"/>
-                    Seattle, WA
-                </span>
-
-                <ContactGroup imgSource="%PUBLIC_URL%/icons/logo-linkedin.svg"
+                <ContactGroup imgSource="../../public/icons/logo-linkedin.svg"
                               imgDescription="View Thomas Berg's profile on LinkedIn"
                               url="https://www.linkedin.com/in/t-berg/"
                               text="LinkedIn"/>
 
-                <ContactGroup imgSource="%PUBLIC_URL%/icons/GitHub-Mark.eps"
+                <ContactGroup imgSource="../../public/icons/GitHub-Mark.eps"
                               imgDescription="View brystmar's repository on GitHub"
                               url="https://github.com/brystmar/"
                               text="GitHub"/>
 
+                <span className="contact-group">
+                    <img src="../../public/icons/map-marked-alt-solid.svg"
+                         alt="Where Thomas lives"/>
+                    Seattle, WA
+                </span>
             </p>
         </section>
     )
